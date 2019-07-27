4b4adfd77bb8b93a6c2275709321b862b860c7cc
denysLavrienov <denys.lavrienov@teamdev.com>
2019-07-27 11:11:43 +0300

    Create index.html and index.js files, containing view to select animal classes.

---
+++ 2019-07-23-intro/app/view/index.js
@@ -0,0 +1,79 @@
+
+const root = document.getElementById("root");
+let input = document.createElement("input");
+
+input.placeholder = "Name";
+input.style.marginRight = "50px";
+root.appendChild(input);
+
+let select = document.createElement("select");
+
+
+let optionCat = document.createElement("option");
+optionCat.textContent = "Cat";
+
+let optionDog = document.createElement("option");
+optionDog.textContent = "Dog";
+
+let optionEagle = document.createElement("option");
+optionEagle.textContent = "Eagle";
+
+let optionElephant = document.createElement("option");
+optionElephant.textContent = "Elephant";
+
+let optionHorse = document.createElement("option");
+optionHorse.textContent = "Horse";
+
+let optionHuman = document.createElement("option");
+optionHuman.textContent = "Human";
+
+let optionLion = document.createElement("option");
+optionLion.textContent = "Lion";
+
+let optionParrot = document.createElement("option");
+optionParrot.textContent = "Parrot";
+
+let optionPig = document.createElement("option");
+optionPig.textContent = "Pig";
+
+let optionSalmon = document.createElement("option");
+optionSalmon.textContent = "Salmon";
+
+let optionTiger = document.createElement("option");
+optionTiger.textContent = "Tiger";
+
+
+
+select.appendChild(optionCat);
+select.appendChild(optionDog);
+select.appendChild(optionEagle);
+select.appendChild(optionElephant);
+select.appendChild(optionHorse);
+select.appendChild(optionHuman);
+select.appendChild(optionLion);
+select.appendChild(optionParrot);
+select.appendChild(optionPig);
+select.appendChild(optionSalmon);
+select.appendChild(optionTiger);
+
+select.selected = optionCat;
+
+root.appendChild(select);
+
+
+let addButton = document.createElement("button");
+addButton.textContent = "Add";
+addButton.style.marginLeft = "50px";
+
+root.appendChild(addButton);
+
+
+
+
+
+
+
+
+
+
+document.createElement("ul");
